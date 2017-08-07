1b369d3fcda9baf35403ed4fdad4e7a2ff0475c4
jacklynhma <jacklynhma@gmail.com>
2017-08-06 23:32:25 -0400

    working tests for logic

--- react/test/containers/GamesShowContainerSpec.js
+++ react/test/containers/GamesShowContainerSpec.js
@@ -1,25 +1,13 @@
 import GamesShowContainer from '../../src/containers/GamesShowContainer';
+import PieceTile from '../../src/components/PieceTile';
+import NavBar from '../../src/components/NavBar';
 
 describe('GamesShowContainer', () => {
   let wrapper;
-  let game = [{
-    id: 1,
-    name: "Testing",
-    state_of_piece:
-    [
-    [nil, "B", nil, "B", nil, "B", nil, "B"],
-    ["B", nil, "B", nil, "B", nil , "B", nil],
-    [nil, "B", nil, "B", nil, "B", nil, "B"],
-    [nil, nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil, nil],
-    ["R", nil, "R", nil, "R", nil, "R", nil],
-    [nil, "R", nil, "R", nil, "R", nil, "R"],
-    ["R", nil, "R", nil, "R", nil, "R", nil]
-  ]
-
-  }]
-
   beforeEach(() => {
-    
+      wrapper = mount( <GamesShowContainer />)
+  });
+  it('should have state board', () => {
+  expect(wrapper).toHaveState('coordinates')
   })
-})
+});
