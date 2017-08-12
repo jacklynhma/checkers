90908270ea6ac98433b9c4771210faa8fd6b9579
jacklynhma <jacklynhma@gmail.com>
2017-08-12 18:18:12 -0400

    deleted piecetile

--- react/test/components/PieceTileSpec.js
+++ react/test/components/PieceTileSpec.js
@@ -1,19 +1,19 @@
-import PieceTile from '../../src/components/PieceTile';
-
-describe('PieceTile', () =>{
-  let wrapper;
-  beforeEach(()=> {
-    jasmineEnzyme();
-
-    wrapper = mount(
-      <PieceTile
-        piece = "R"
-      />
-    )
-  })
+// import PieceTile from '../../src/components/PieceTile';
+//
+// describe('PieceTile', () =>{
+//   let wrapper;
+//   beforeEach(()=> {
+//     jasmineEnzyme();
+//
+//     wrapper = mount(
+//       <PieceTile
+//         piece = "R"
+//       />
+//     )
+//   })
 
   // it('should render div tag', () => {
-  // 
+  //
   //   expect(wrapper.find('div.redpiece')).toBePresent()
   //   // expect(wrapper.find('p').text()).toBe("R")
   // })
